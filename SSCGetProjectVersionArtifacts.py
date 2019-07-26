c4f3464b169178e438bfbec7583d00acb9257b05
zach2017 <zaclewis@gmail.com>
2019-07-26 06:58:49 -0500

    k

---
+++ SSCGetProjectVersionArtifacts.py
@@ -0,0 +1,13 @@
+import requests
+
+auth_token='YzcyYWE0NGEtYWEzOS00NjM0LTkyMmMtY2E0OWMwODc4YTcy'
+#c72aa44a-aa39-4634-922c-ca49c0878a72
+hed = {'Authorization': 'FortifyToken ' + auth_token, 'Content-Type': 'application/json', 'Accept': 'application/json' }
+#data = { "applicationName": "ssscapp" }
+
+url = 'http://localhost:8080/ssc/api/v1/projectVersions/2/artifacts'
+
+#response = requests.post(url, json=data, headers=hed)
+response = requests.get(url, headers=hed)
+
+print(response.json())
